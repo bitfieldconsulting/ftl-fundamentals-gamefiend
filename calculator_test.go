98600e019192033edcd4adbc8ba9ff96fd2c5216
Quinn Murphy <gamefiend@github.com>
2020-10-23 07:59:04 -0400

    exercise 5 test tables

--- calculator_test.go
+++ calculator_test.go
@@ -4,30 +4,71 @@ import (
 	"calculator"
 	"testing"
 )
+type addTest []struct {
+	a float64 //input
+	b float64 //input
+	want float64 //output
+	name string //label
+}
+
+type subtractTest []struct {
+	a float64 // input
+	b float64 //input
+	want float64 //output
+	name string //label
+}
+
+type multiplyTest []struct {
+	a float64 //input
+	b float64 //input
+	want float64 //output
+	name string //label
+}
+
+var addTests = addTest{
+	{1, 1, 2, "Addition of two small integers"},
+	{-10, -20, -30, "Addition of two negative integer"},
+	{4.5, 4.5, 9, "Addition to two floating points"},
+}
+
+var subtractTests = subtractTest{
+	{ 5, -5, 0, "Subtraction of positive and a negative integer"},
+	{1, 1, 0, "Subtraction of two positive integers"},
+	{10.5, 1.0, 9.5, "Subtraction of two floating points"},
+}
+
+var multiplyTests = multiplyTest{
+	{4, 12, 48, "Multiplication of two positive integers"},
+	{4, -12, -48, "Multiplication of a positive and negative integer"},
+	{.5, 1, .5, "Multiplication of a floating point and an integer"},
+}
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	for _, tt := range addTests {
+		got := calculator.Add(tt.a, tt.b)
+		if tt.want != got {
+			t.Errorf("%s: want %f, got %f",tt.name, tt.want, got)
+		}
 	}
 }
 
 func TestSubtract(t *testing.T) {
 	t.Parallel()
-	var want float64 = 2
-	got := calculator.Subtract(4, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	for _, tt := range subtractTests {
+		got := calculator.Subtract(tt.a, tt.b)
+		if tt.want != got {
+			t.Errorf("%s: want %f, got %f",tt.name, tt.want, got)
+		}
 	}
 }
 
 func TestMultiply(t *testing.T) {
 	t.Parallel()
-	var want float64 = 10
-	got := calculator.Multiply(5, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	for _, tt := range multiplyTests {
+		got := calculator.Multiply(tt.a, tt.b)
+		if tt.want != got {
+			t.Errorf("%s: want %f, got %f",tt.name, tt.want, got)
+		}
 	}
 }
