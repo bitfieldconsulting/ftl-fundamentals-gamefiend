3e27a3dcd8f09a3e3b7e59ea5c63036cb5cbd5cc
Quinn Murphy <gamefiend@github.com>
2020-12-04 16:00:59 -0500

    added variadic functions

--- calculator.go
+++ calculator.go
@@ -8,29 +8,60 @@ import (
 )
 
 // Add takes two numbers and returns the result of adding them together.
-func Add(a, b float64) float64 {
-	return a + b
+func Add(nums ...float64) float64 {
+	total := nums[0]
+	for i, d := range nums {
+		if i == 0 {
+			continue
+		}
+		total += d
+	}
+	return total
 }
 
 // Subtract takes two numbers and returns the result of subtracting the second
 // from the first.
-func Subtract(a, b float64) float64 {
-	return a - b
+func Subtract(nums ...float64) float64 {
+	total := nums[0]
+	for i, d := range nums {
+		if i == 0 {
+			continue
+		}
+		total -= d
+	}
+	return total
 }
 
 // Multiply takes two numbers and returns the result of multiplying the first by
 // the second.
-func Multiply(a, b float64) float64 {
-	return a * b
+func Multiply(nums ...float64) float64 {
+	total := nums[0]
+	for i, d := range nums {
+		if i == 0 {
+			continue
+		}
+		total *= d
+	}
+	return total
+
 }
 
 // Divide takes two numbers and returns the result of dividing the first by the
 // second, or an error if the second value is zero.
-func Divide(a, b float64) (float64, error) {
-	if b == 0 {
-		return 0, errors.New("division by zero is undefined")
+func Divide(nums ...float64) (float64, error) {
+	total := nums[0]
+	for i, d := range nums {
+		// skip the first element, which is the total
+		if i == 0 {
+			continue
+		}
+		// return error if divisor is 0
+		if d == 0 {
+			return 0, errors.New("division by zero is undefined")
+		}
+		total /= d
 	}
-	return a / b, nil
+	return total, nil
 }
 
 func Sqrt(a float64) (float64, error) {
--- calculator_test.go
+++ calculator_test.go
@@ -8,61 +8,80 @@ import (
 func TestAddSubtractMultiply(t *testing.T) {
 	t.Parallel()
 	tcs := []struct {
-		a    float64 // input
-		b    float64 // input
+		nums []float64
 		want float64 // output
 		name string  // label
-		f    func(float64, float64) float64
+		f    func(nums ...float64) float64
 	}{
 		{
 			name: "Addition of two small integers",
-			a:    1, b: 1, want: 2,
-			f: calculator.Add,
+			nums: []float64{1, 1},
+			want: 2,
+			f:    calculator.Add,
 		},
 		{
 			name: "Addition of two negative integer",
-			a:    -10, b: -20, want: -30,
-			f: calculator.Add,
+			nums: []float64{-10, -20},
+			want: -30,
+			f:    calculator.Add,
 		},
 		{
 			name: "Addition of two floating points",
-			a:    4.5, b: 4.5, want: 9,
-			f: calculator.Add,
+			nums: []float64{4.5, 4.5},
+			want: 9,
+			f:    calculator.Add,
+		},
+		{
+			name: "Addition of four floating points",
+			nums: []float64{4.5, 4.5, 4.5, 4.5},
+			want: 18,
+			f:    calculator.Add,
 		},
 		{
 			name: "Subtraction of positive and a negative integer",
-			a:    5, b: -5, want: 10,
-			f: calculator.Subtract,
+			nums: []float64{5, -5},
+			want: 10,
+			f:    calculator.Subtract,
 		},
 		{
 			name: "Subtraction of two positive integers",
-			a:    1, b: 1, want: 0,
-			f: calculator.Subtract,
+			nums: []float64{1, 1},
+			want: 0,
+			f:    calculator.Subtract,
 		},
 		{
 			name: "Subtraction of two floating points",
-			a:    10.5, b: 1.0, want: 9.5,
-			f: calculator.Subtract,
+			nums: []float64{10.5, 1.0},
+			want: 9.5,
+			f:    calculator.Subtract,
 		},
 		{
 			name: "Multiplication of two positive integers",
-			a:    4, b: 12, want: 48,
-			f: calculator.Multiply,
+			nums: []float64{4, 12},
+			want: 48,
+			f:    calculator.Multiply,
 		},
 		{
 			name: "Multiplication of a positive and negative integer",
-			a:    0.5, b: 1.0, want: 0.5,
-			f: calculator.Multiply,
+			nums: []float64{0.5, 1.0},
+			want: 0.5,
+			f:    calculator.Multiply,
 		},
-		{	name: "Multiplication of a floating point and an integer of two floating points",
-			a:    10.5, b: 1.0, want: 10.5,
-			f: calculator.Multiply,
+		{name: "Multiplication of two floating points",
+			nums: []float64{10.5, 1.0},
+			want: 10.5,
+			f:    calculator.Multiply,
+		},
+		{name: "Multiplication of three floating points",
+			nums: []float64{10.5, 1.0, 3.0},
+			want: 31.5,
+			f:    calculator.Multiply,
 		},
 	}
 
 	for _, tc := range tcs {
 		t.Run(tc.name, func(t *testing.T) {
-			got := tc.f(tc.a, tc.b)
+			got := tc.f(tc.nums...)
 			if tc.want != got {
 				t.Errorf("want %f, got %f", tc.want, got)
 			}
@@ -74,17 +93,20 @@ func TestDivide(t *testing.T) {
 	t.Parallel()
 	tcs := []struct {
 		name        string
-		a, b        float64
+		nums        []float64
 		want        float64
 		errExpected bool
 	}{
-		{name: "Happy", a: 2, b: 1, want: 2},
-		{name: "Zero divided by...", a: 0, b: 1, want: 0},
-		{name: "Division by zero", a: 1, b: 0, want: 999, errExpected: true},
+		{name: "Happy", nums: []float64{2, 1}, want: 2},
+		{name: "Happy with three divisors", nums: []float64{20, 5, 2}, want: 2},
+		{name: "Zero divided by...", nums: []float64{0, 20}, want: 0},
+		{name: "Zero divided by...with three divisors", nums: []float64{0, 10, 20}, want: 0},
+		{name: "Division by zero", nums: []float64{1, 0}, want: 999, errExpected: true},
+		{name: "Division by zero with three divisors", nums: []float64{10, 5, 0}, want: 999, errExpected: true},
 	}
 	for _, tc := range tcs {
 		t.Run(tc.name, func(t *testing.T) {
-			got, err := calculator.Divide(tc.a, tc.b)
+			got, err := calculator.Divide(tc.nums...)
 			errReceived := err != nil
 			if tc.errExpected != errReceived {
 				t.Fatalf("unexpected error status %v", err)
